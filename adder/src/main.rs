9ef1a2cd61a91f95fd2846723627d3055848066b
Jaroslaw Cichon <dev@iana.io>
2023-06-15 07:45:13 +0000

    init

--- adder/src/main.rs
+++ adder/src/main.rs
@@ -1,7 +1,7 @@
 use add_lib1;
 
 fn main() {
-    let num = 10;
-    println!("Hello, world from Rust Workspaces {num} plus one is {}!", add_lib1::add_lib1(num));
+    let num = 6;
+    println!("THE FIRST AMENDMENT OF IANA SYSTEMS CODE OF CONDUCT {num} is bad, {} is good. x = 6; where everyone is bad by a default because everyone could be a bad person. It depeneds on person's relations, his connections and the environment that person lives. The most corrupted env give much potential for that person to be bad. IANA SYSTEMS algorithm has a hope for everyone to become a good person so, x+1=hope. and it gives 7, where 7 is always good! VALUE = MOTTO: 6+1", add_lib1::add_lib1(num));
 }
 
